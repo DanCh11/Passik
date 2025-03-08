5a99079543d8f54d9a1d2499ce11f53c686f7dfb
DanCh11 <dan.chiriac1453@gmail.com>
2025-03-08 23:18:49 +0100

    created auth service

--- passik/application/src/main/java/service/security/AuthenticationService.java
+++ passik/application/src/main/java/service/security/AuthenticationService.java
@@ -1,4 +1,42 @@
 package service.security;
 
-public class Authentication {
+import de.daycu.passik.model.security.MasterLogin;
+import de.daycu.passik.model.security.MasterPassword;
+import lombok.AllArgsConstructor;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+
+/**
+ *  Performs authentication by instantiating the users using UserSecurityManager
+ *  with injected UserRealm class.
+ *
+ *  @return successful authentication if the user is found. Else throws an exception.
+ */
+@AllArgsConstructor
+public class AuthenticationService {
+
+    private MasterLogin masterLogin;
+    private MasterPassword masterPassword;
+    private UserRealm userRealm;
+
+    public AuthenticationResult authenticate() {
+        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(
+                masterLogin.toString(), masterPassword.toString());
+        Subject user = new PassikSecurityManager(userRealm).configure();
+
+        try {
+            user.login(usernamePasswordToken);
+            return new AuthenticationResult(true, "Login was performed successfully.");
+        } catch (UnknownAccountException uae) {
+            return new AuthenticationResult(false, "Account is not registered.");
+        } catch (IncorrectCredentialsException ice) {
+            return new AuthenticationResult(false,
+                    "Invalid credentials provided. Please check your username and password.");
+        } catch (AuthenticationException ae) {
+            return new AuthenticationResult(false, "Unknown error.");
+        }
+    }
 }
