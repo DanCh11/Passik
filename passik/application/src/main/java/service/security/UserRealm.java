be29a3e6292f0db98b8d8b371962d4df433920e4
DanCh11 <dan.chiriac1453@gmail.com>
2025-03-08 23:18:15 +0100

    created user realm

--- passik/application/src/main/java/service/security/UserRealm.java
+++ passik/application/src/main/java/service/security/UserRealm.java
@@ -1,4 +1,37 @@
 package service.security;
 
-public class UserRealm {
+import lombok.AllArgsConstructor;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.realm.AuthenticatingRealm;
+
+import java.util.HashMap;
+
+/**
+ * Creates a realm to interact with users' authentication data.
+ */
+@AllArgsConstructor
+public class UserRealm extends AuthenticatingRealm {
+
+    private HashMap<String, String> users;
+
+    /**
+     * Retrieves authentication information of the user.
+     * @param token the authentication token containing the user's principal and credentials.
+     *
+     * @return authentication info
+     * @throws AuthenticationException if the authentication failed.
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        String username = (String) token.getPrincipal();
+
+        if (!users.containsKey(username)) {
+            System.out.println("There is no such user");
+        }
+
+        return new SimpleAuthenticationInfo(username, users.get(username), getName());
+    }
 }
