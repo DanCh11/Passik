061ab7d5c9f2a4098a34dceac08a2cc7d48c5101
DanCh11 <dan.chiriac1453@gmail.com>
2025-03-14 16:18:48 +0100

    refactored tests to use mockito

--- passik/application/src/test/java/security/AuthenticationServiceTest.java
+++ passik/application/src/test/java/security/AuthenticationServiceTest.java
@@ -1,51 +1,64 @@
 package security;
 
+import de.daycu.passik.model.security.Master;
 import de.daycu.passik.model.security.MasterLogin;
 import de.daycu.passik.model.security.MasterPassword;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import port.out.persistance.MasterRepository;
+import service.security.AuthenticationResult;
 import service.security.AuthenticationService;
-import service.security.UserRealm;
-
-import java.util.HashMap;
+import service.security.MasterRealm;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class AuthenticationServiceTest {
 
     private MasterLogin masterLogin;
     private MasterPassword masterPassword;
-    private UserRealm userRealm;
+    private MasterRealm masterRealm;
+    @Mock
+    private MasterRepository masterRepository;
     private AuthenticationService authenticationService;
 
     @BeforeEach
     void init() {
-        masterLogin = new MasterLogin("da");
-        masterPassword = new MasterPassword("123");
-        HashMap<String, String> users = new HashMap<>();
-        users.put(masterLogin.toString(), masterPassword.toString());
-        userRealm = new UserRealm(users);
+        masterLogin = new MasterLogin("masterLogin");
+        masterPassword = new MasterPassword("masterPassword");
+        Master master = new Master(masterLogin, masterPassword);
+        masterRealm = new MasterRealm(masterRepository);
+        lenient().when(masterRepository.getMasterByLogin(masterLogin)).thenReturn(master);
     }
 
     @Test
     @DisplayName("Testing successful authentication")
     public void testSuccessfulAuthentication() {
-        authenticationService = new AuthenticationService(masterLogin, masterPassword, userRealm);
-        assertTrue(authenticationService.authenticate().success());
-        assertEquals("Login was performed successfully.", authenticationService.authenticate().message());
+        authenticationService = new AuthenticationService(masterRealm);
+        AuthenticationResult result = authenticationService.authenticate(masterLogin, masterPassword);
 
+        assertTrue(result.success());
+        assertEquals("Login was performed successfully.", result.message());
     }
 
     @Test
     @DisplayName("Testing account wrong credentials")
     public void testAccountWrongCredentials() {
-        masterLogin = new MasterLogin("da");
+        masterLogin = new MasterLogin("masterLogin");
         masterPassword = new MasterPassword("1234");
-        authenticationService = new AuthenticationService(masterLogin, masterPassword, userRealm);
-        assertFalse(authenticationService.authenticate().success());
+        authenticationService = new AuthenticationService(masterRealm);
+        AuthenticationResult result = authenticationService.authenticate(masterLogin, masterPassword);
+
+        assertFalse(result.success());
         assertEquals(
                 "Invalid credentials provided. Please check your username and password.",
-                authenticationService.authenticate().message());
+                result.message()
+        );
     }
 }
