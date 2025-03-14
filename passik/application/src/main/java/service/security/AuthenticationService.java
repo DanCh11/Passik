adcc69afcf84be83f9a230519bdeb344bc23a222
DanCh11 <dan.chiriac1453@gmail.com>
2025-03-14 16:18:06 +0100

    refactored the auth service

--- passik/application/src/main/java/service/security/AuthenticationService.java
+++ passik/application/src/main/java/service/security/AuthenticationService.java
@@ -3,11 +3,10 @@ package service.security;
 import de.daycu.passik.model.security.MasterLogin;
 import de.daycu.passik.model.security.MasterPassword;
 import lombok.AllArgsConstructor;
-import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.IncorrectCredentialsException;
-import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
+import port.in.security.AuthenticationUseCase;
 
 /**
  *  Performs authentication by instantiating the users using UserSecurityManager
@@ -16,27 +15,22 @@ import org.apache.shiro.subject.Subject;
  *  @return successful authentication if the user is found. Else throws an exception.
  */
 @AllArgsConstructor
-public class AuthenticationService {
+public class AuthenticationService implements AuthenticationUseCase {
 
-    private MasterLogin masterLogin;
-    private MasterPassword masterPassword;
-    private UserRealm userRealm;
+    private MasterRealm masterRealm;
 
-    public AuthenticationResult authenticate() {
+    public AuthenticationResult authenticate(MasterLogin masterLogin, MasterPassword masterPassword) {
+
         UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(
-                masterLogin.toString(), masterPassword.toString());
-        Subject user = new PassikSecurityManager(userRealm).configure();
+                masterLogin.value(), masterPassword.value());
+        Subject user = new PassikSecurityManager(masterRealm).configure();
 
         try {
             user.login(usernamePasswordToken);
             return new AuthenticationResult(true, "Login was performed successfully.");
-        } catch (UnknownAccountException uae) {
-            return new AuthenticationResult(false, "Account is not registered.");
         } catch (IncorrectCredentialsException ice) {
             return new AuthenticationResult(false,
                     "Invalid credentials provided. Please check your username and password.");
-        } catch (AuthenticationException ae) {
-            return new AuthenticationResult(false, "Unknown error.");
         }
     }
 }
