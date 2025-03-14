1d6089c49f6e7960625e4589b33c8e208235bd85
DanCh11 <dan.chiriac1453@gmail.com>
2025-03-14 16:19:07 +0100

    a record to represent the user of the app

--- passik/model/src/main/java/de/daycu/passik/model/security/Master.java
+++ passik/model/src/main/java/de/daycu/passik/model/security/Master.java
@@ -1,4 +1,11 @@
 package de.daycu.passik.model.security;
 
-public record Master() {
+import java.util.Objects;
+
+public record Master(MasterLogin masterLogin, MasterPassword masterPassword) {
+
+    public Master {
+        Objects.requireNonNull(masterLogin, "'masterLogin' shall not be null");
+        Objects.requireNonNull(masterPassword, "'masterPassword' shall not be null");
+    }
 }
