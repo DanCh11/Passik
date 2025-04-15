acd451cca0b488fc5546e1143ddfd357078d15b6
DanCh11 <dan.chiriac1453@gmail.com>
2025-04-15 16:29:24 +0200

    added new methods and refactored old ones in repos

--- passik/application/src/main/java/port/in/vault/DigitalAccountUseCase.java
+++ passik/application/src/main/java/port/in/vault/DigitalAccountUseCase.java
@@ -1,11 +1,15 @@
 package port.in.vault;
 
+import de.daycu.passik.model.auth.MasterId;
 import de.daycu.passik.model.vault.DigitalAccount;
 import de.daycu.passik.model.vault.DigitalServiceName;
 import service.vault.DigitalAccountDeletionResult;
 
+import java.util.List;
+
 public interface DigitalAccountUseCase {
     DigitalAccount save(DigitalAccount digitalAccount);
+    List<DigitalAccount> findAllByMasterId(MasterId masterId);
     DigitalAccount update(DigitalServiceName digitalServiceName, DigitalAccount digitalAccount);
     DigitalAccountDeletionResult delete(DigitalAccount digitalAccount);
 
--- passik/application/src/main/java/port/out/persistance/DigitalAccountRepository.java
+++ passik/application/src/main/java/port/out/persistance/DigitalAccountRepository.java
@@ -2,13 +2,14 @@ package port.out.persistance;
 
 import java.util.List;
 
+import de.daycu.passik.model.auth.MasterId;
 import de.daycu.passik.model.vault.DigitalAccount;
 import de.daycu.passik.model.vault.DigitalServiceName;
 
 public interface DigitalAccountRepository {
     DigitalAccount save(DigitalAccount digitalAccount);
     void delete(DigitalAccount digitalAccount);
-    DigitalAccount update(DigitalServiceName digitalServiceName, DigitalAccount updatedDigitalAccount);
-    List<DigitalAccount> findAll();
-    DigitalAccount findByDigitalServiceName(DigitalServiceName digitalServiceName);
+    DigitalAccount update(DigitalAccount updatedDigitalAccount);
+    List<DigitalAccount> findAllByMasterId(MasterId masterId);
+    DigitalAccount findByDigitalServiceName(MasterId masterId, DigitalServiceName digitalServiceName);
 }
