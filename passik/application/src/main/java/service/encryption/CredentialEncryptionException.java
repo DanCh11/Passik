a80ab0f48c4c8a9b09bfaa1c1b36beceb40a3869
DanCh11 <dan.chiriac1453@gmail.com>
2025-04-26 10:49:44 +0200

    renamed the exception

--- passik/application/src/main/java/service/encryption/CredentialEncryptionException.java
+++
@@ -1,7 +0,0 @@
-package service.encryption;
-
-public class CredentialEncryptionException extends RuntimeException {
-    public CredentialEncryptionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
